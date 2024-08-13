5733a8f8a4897882cd63a237a8a1f92fa31ecda9
Aneesh <136454406+lhcee3@users.noreply.github.com>
2024-08-14 00:11:16 +0530

    external upload

---
+++ data/index.ts
@@ -0,0 +1,220 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize collaboration and fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Web Developer with a passion for Machine Learning.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a ML Based Automation library",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Tasty Studio ",
+    des: "A Interactive Website with a clean UI for entry level restraunts. ",
+    img: "/p1.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "https://tasty-studio.netlify.app",
+  },
+  {
+    id: 2,
+    title: "MDPS with Machine Learning",
+    des: "Multiple Disease Prediction System made with Machine Learning and Support Vector Machine(SVM)",
+    img: "/p2.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "aneeshdiabetespred.streamlit.app/",
+  },
+  {
+    id: 3,
+    title: " Astronomical Data Visualization ",
+    des: "Here I collect astronomical data and use different visualization methods from Matplotlib and Seaborn Libraries to plot something called a HR Diagram.",
+    img: "/p3.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "https://github.com/lhcee3/Astronomical-Data-Visualization",
+  },
+  {
+    id: 4,
+    title: "Weapon Detection",
+    des: "As a project that helped me win 2 hackathons. This is an weapon detection model made using Open CV. ",
+    img: "/p4.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "https://github.com/lhcee3/Weapon-Detection-Opencv",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Aneesh was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Aneesh's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Aneesh is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaCum Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Aneesh was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Aneesh's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Aneesh is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaCum Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Aneesh was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Aneesh's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Aneesh is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaCum Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Aneesh was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Aneesh's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Aneesh is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaCum Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Aneesh was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Aneesh's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Aneesh is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaCum Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Full Time Student",
+    desc: "CS Sophomore at a not so good college. Also a self taught developer.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Web App Dev ",
+    desc: "Designed and developed Web apps for both iOS & Android platforms using React Native.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Machine Learning Engineer",
+    desc: "Developed and also helped people develop quality machine learning models with great accuracy.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Video Editor",
+    desc: "A tiny hobby and side hustle keeping my will to learn more active everyday.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    href: "https://github.com/lhcee3",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    href: "https://x.com/lhcee3",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    href: "https://linkedin.com/in/saianeeshg90",
+  },
+];
